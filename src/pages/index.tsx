cc18917a658e0136ffb54ab316db2a61b5d501c5
Mirza Raheel <raheel1195@gmail.com>
2025-11-24 09:26:47 +0500

    billing check added on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Page,
   Card,
@@ -11,12 +11,29 @@ import {
   Text,
   Banner,
 } from '@shopify/polaris';
-import { useRouter } from 'next/router';
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function IndexPage() {
-  const [shopName, setShopName] = useState('');
+  const [shopName, setShopName] = useState('new-dev-store-12324471.myshopify.com');
   const [error, setError] = useState('');
   const router = useRouter();
+  const [billingActive, setBillingActive] = useState(false);
+
+  useEffect(() => {
+    async function checkBilling() {
+      try {
+        const b = await axios.get<{ status: boolean }>(`/api/billingStatus?shop=${shopName}`);
+        setBillingActive(b.data.status);
+      }
+      catch (err) {
+        console.error(err);
+        setError("Failed to load data");
+      }
+    }
+    checkBilling();
+
+  }, [shopName]);
 
   const handleInstallClick = () => {
     if (!shopName) {
@@ -31,48 +48,61 @@ export default function IndexPage() {
   return (
     <Page title="Welcome to the Shopify App">
       <Layout>
-        <Layout.Section>
-          {error && (
-            <Banner tone="critical">
-              <Text as="p">{error}</Text>
-            </Banner>
-          )}
+        {!billingActive &&
+          <>
+            <Layout.Section>
+              {error && (
+                <Banner tone="critical">
+                  <Text as="p">{error}</Text>
+                </Banner>
+              )}
 
-          <Card>
-            <BlockStack gap="300">
-              <Text as="h2" variant="headingMd">
-                Install Your App
-              </Text>
+              <Card>
+                <BlockStack gap="300">
+                  <Text as="h2" variant="headingMd">
+                    Install Your App
+                  </Text>
 
-              <Text as="p">
-                Enter your store URL to begin the installation process.
-              </Text>
+                  <Text as="p">
+                    Enter your store URL to begin the installation process.
+                  </Text>
 
-              <InlineStack gap="200">
-                <TextField
-                  label="Shopify Store URL"
-                  value={shopName}
-                  onChange={setShopName}
-                  placeholder="example.myshopify.com"
-                  autoComplete="off"
-                />
+                  <InlineStack gap="200">
+                    <TextField
+                      label="Shopify Store URL"
+                      value={shopName}
+                      onChange={setShopName}
+                      placeholder="example.myshopify.com"
+                      autoComplete="off"
+                    />
 
-                <Button variant="primary" onClick={handleInstallClick}>
-                  Install App
-                </Button>
-              </InlineStack>
+                    <Button variant="primary" onClick={handleInstallClick}>
+                      Install App
+                    </Button>
+                  </InlineStack>
+                </BlockStack>
+              </Card>
+            </Layout.Section>
+            <Layout.Section>
+              <BlockStack gap="200">
+                <Text as="p">Alternatively, test with a hardcoded link:</Text>
+                <Link url="/api/auth?shop=new-dev-store-12324471.myshopify.com">
+                  Install App for example store
+                </Link>
+              </BlockStack>
+            </Layout.Section>
+          </>
+        }
+        {billingActive &&
+          <Layout.Section>
+            <BlockStack gap="200">
+
+              <Link url="/dashboard?shop=${shop}">
+                View Dashboard
+              </Link>
             </BlockStack>
-          </Card>
-        </Layout.Section>
-
-        <Layout.Section>
-          <BlockStack gap="200">
-            <Text as="p">Alternatively, test with a hardcoded link:</Text>
-            <Link url="/api/auth?shop=new-dev-store-12324471.myshopify.com">
-              Install App for example store
-            </Link>
-          </BlockStack>
-        </Layout.Section>
+          </Layout.Section>
+        }
       </Layout>
     </Page>
   );
