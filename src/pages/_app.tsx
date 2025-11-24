68b25ca98dc41ad4fe2c7ad122960db299375c13
Mirza Raheel <raheel1195@gmail.com>
2025-11-24 17:17:15 +0500

    host issue fixing adding host from env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import '@shopify/polaris/build/esm/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const host = process.env.HOST || "";
+  const host = process.env.NEXT_PUBLIC_HOST || "";
   const config = {
     apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY || "",
     host,
