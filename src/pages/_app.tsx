48d9eec859ed5dd3765b9288acbb7b8482b49934
Mirza Raheel <raheel1195@gmail.com>
2025-11-26 17:16:06 +0500

    added shop in provider from cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,11 @@ import enTranslations from "@shopify/polaris/locales/en.json";
 import { ShopProvider } from "@/context/ShopContext";
 
 function MyApp({ Component, pageProps }: AppProps & { pageProps: { shop?: string } }) {
+  const shopFromCookie = typeof window !== "undefined"
+    ? document.cookie.split("; ").find(c => c.startsWith("shop="))?.split("=")[1]
+    : undefined;
   return (
-    <ShopProvider initialShop={pageProps.shop}>
+    <ShopProvider initialShop={pageProps.shop || shopFromCookie}>
       <AppProvider i18n={enTranslations}>
         <Component {...pageProps} />
       </AppProvider>
